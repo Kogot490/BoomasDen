0698e930ff65d265d77ba150cc81da06ffcff960
Kogot490 <51406121+Kogot490@users.noreply.github.com>
2023-04-04 15:01:42 -0600

    Add files via upload
    Added section ID calc and some navigation

---
+++ boomajs.js
@@ -0,0 +1,15 @@
+
+/* const userName = document.getElementById("userName");
+const userNameContent = userName.value;
+console.log(userNameContent);
+
+function calculateIdSum() {
+    i = 0
+
+while (i < userNameContent.length) {
+	idSum += userNameContent.charCodeAt(i) % 10;
+	i++;
+}}
+ */
+
+// added different .js for each page so 
